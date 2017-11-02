70e28c781014d95be7ef96fb13c66d0c9218283a
Jake Burden <jake@doge.haus>
2017-11-01 22:32:17 -0400

    adds tests for the end event and internal changes to the .close() and .open() methods

--- test.js
+++ test.js
@@ -12,8 +12,21 @@ SSE
   .on('error', function (err) {
     console.error(err)
   })
+  .on('end', function () {
+    console.info('SSE has ended')
+  })
 
-// close SSE after 5 seconds 
+// close SSE after 5 seconds
 setTimeout(function () {
   SSE.close()
 }, 5000)
+
+// open SSE after 7 seconds
+setTimeout(function () {
+  SSE.open()
+}, 7000)
+
+// close SSE after 10 seconds
+setTimeout(function () {
+  SSE.close()
+}, 10000)
